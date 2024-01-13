dfe3cabb9d8787db0069a3ac4de8a4821deedb85
Kovalchuk63 <vov4uk4763@gmail.com>
2024-01-13 11:13:40 +0200

    edit_4

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 import mongoose from "mongoose";
 import app from "./app.js";
 
-const DB_HOST =
-  "mongodb+srv://Mushka4763:Mushka4763@cluster0.wkadonx.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOS, PORT = 3000 } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
     app.listen(3000, () => {
       console.log(
-        `Database connection successful. Server running on port: 3000 `
+        `Database connection successful. Server running on port: ${PORT} `
       );
     });
   })
