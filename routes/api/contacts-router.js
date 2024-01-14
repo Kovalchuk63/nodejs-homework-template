f76cd7d88b197b9871f94a93378928567becb933
Kovalchuk63 <vov4uk4763@gmail.com>
2024-01-14 20:09:48 +0200

    edit_1

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -1,23 +1,50 @@
 import express from "express";
-import {authenticate, isEmptyBody, isValidId} from "../../middlewares/index.js";
+import {
+  authenticate,
+  isEmptyBody,
+  isValidId,
+  upload,
+} from "../../middlewares/index.js";
 import contactController from "../../controllers/contacts-controller.js";
-import { contactsAddSchema, updateFavoriteSchema } from "../../models/contacts/index.js";
-import { validateBody } from "../../decorators/index.js"
+import {
+  contactsAddSchema,
+  updateFavoriteSchema,
+} from "../../models/contacts/index.js";
+import { validateBody } from "../../decorators/index.js";
 
-const contactsRouter = express.Router()
+const contactsRouter = express.Router();
 
 contactsRouter.use(authenticate);
 
-contactsRouter.get('/', contactController.getAllContacts);
+contactsRouter.get("/", contactController.getAllContacts);
 
-contactsRouter.get('/:id', isValidId, contactController.getById);
+contactsRouter.get("/:id", isValidId, contactController.getById);
 
-contactsRouter.post('/', isEmptyBody, validateBody(contactsAddSchema), contactController.add);
+// upload.fields([{name: "poster", maxCount: 8}])
+// upload.array("poster", 8)
+contactsRouter.post(
+  "/",
+  upload.single("poster"),
+  isEmptyBody,
+  validateBody(contactsAddSchema),
+  contactController.add
+);
 
-contactsRouter.delete('/:id', isValidId, contactController.deleteById);
+contactsRouter.delete("/:id", isValidId, contactController.deleteById);
 
-contactsRouter.put('/:id', isValidId, isEmptyBody, validateBody(contactsAddSchema), contactController.updateById);
+contactsRouter.put(
+  "/:id",
+  isValidId,
+  isEmptyBody,
+  validateBody(contactsAddSchema),
+  contactController.updateById
+);
 
-contactsRouter.patch('/:id/favorite', isValidId, validateBody(updateFavoriteSchema), contactController.updateFavoriteById);
+contactsRouter.patch(
+  "/:id/favorite",
+  isValidId,
+  validateBody(updateFavoriteSchema),
+  contactController.updateFavoriteById
+);
 
 export default contactsRouter;
