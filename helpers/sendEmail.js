c02d91d310b9bcb94c69671be083a45caedcbf28
Kovalchuk63 <vov4uk4763@gmail.com>
2024-01-24 20:56:43 +0200

    fix var

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -5,7 +5,7 @@ const { UKR_NET_PASSWORD, UKR_NET_FROM } = process.env;
 
 const nodemailerConfig = {
   host: "smtp.ukr.net",
-  port: 465, // 25, 465, 2525
+  port: 465 /* 25, 465, 2525 */,
   secure: true,
   auth: {
     user: UKR_NET_FROM,
@@ -22,18 +22,18 @@ const sendEmail = (data) => {
 
 export default sendEmail;
 
-//const email = {
-//    to: "tourus81@gmail.com",
-//    from: UKR_NET_FROM,
-//    subject: "Test",
-//    html: "<p> It is working</p>",
-// };
+/* const email = {
+    to: "tourus81@gmail.com",
+    from: UKR_NET_FROM,
+    subject: "Test",
+    html: "<p> It is working</p>",
+ };
 
-//const sendEmail = async () => {
-//   try {
-//     await transport.sendMail(email);
-//     console.log("Email sent successfully");
-//   } catch (error) {
-//     console.log(error.message);
-//   }
-// };
+const sendEmail = async () => {
+   try {
+     await transport.sendMail(email);
+     console.log("Email sent successfully");
+   } catch (error) {
+     console.log(error.message);
+   }
+ }; */
